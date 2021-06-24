08dffea7d2c756ec1b72e14644a4f4345e3b7e18
James Moore <jameswmoore99@gmail.com>
2021-06-24 17:22:16 -0600

    1st part of setting up repo

---
+++ Database/schema.sql
@@ -0,0 +1,55 @@
+-- Useful lines for psql
+
+-- psql -U postgres -h 127.0.0.1 -d questionsandanswers -f ./schema.sql;
+
+-- \copy questions
+-- FROM '/Users/jamesmoore/Documents/Orietation/QnA-API-Service/db/qa/CSVs/questions.csv'
+-- DELIMITER ',' CSV HEADER;
+
+-- ALTER TABLE  reviews
+--   ALTER COLUMN date TYPE TIMESTAMP USING to_timestamp(date / 1000) + ((date % 1000) || ' milliseconds') :: INTERVAL;
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+-- DROP DATABASE IF EXISTS questionsandanswers;
+
+-- CREATE DATABASE questionsandanswers
+--     WITH
+--     OWNER = postgres
+
+-- Tables
+CREATE TABLE questions (
+    questionId BIGSERIAL,
+    productId BIGINT,
+    body text,
+    date BIGINT,
+    askerName text,
+    askerEmail text,
+    reported BIGINT,
+    helpfullness BIGINT,
+    CONSTRAINT questions_pkey PRIMARY KEY (questionId)
+);
+
+
+CREATE TABLE answers (
+    answerID BIGSERIAL,
+    questionID BIGSERIAL,
+    body text,
+    date BIGINT,
+    answerName text,
+    answerEmail text,
+    reported BIGINT,
+    helpfulness BIGINT,
+    CONSTRAINT answers_pkey PRIMARY KEY (answerID)
+);
+
+
+CREATE TABLE answerPhotos (
+    photoID BIGSERIAL,
+    answerID BIGSERIAL,
+    URL text,
+    CONSTRAINT answerPhotos_pkey PRIMARY KEY (photoID),
+    CONSTRAINT answerID FOREIGN KEY (photoID)
+        REFERENCES answers (answerID) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
